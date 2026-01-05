2b9da43e771b3c8fd9534350f6c2722eaa022512
Dragon-learn <haidarhisyam1823@gmail.com>
2026-01-06 01:11:28 +0700

    Add files via upload

---
+++ Kalisumur/data/Kesehatan_Kalisumur_4.js
@@ -0,0 +1,1 @@
+var json_Kesehatan_Kalisumur_4 = {"type":"FeatureCollection","name":"Kesehatan_Kalisumur_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null,"Nama":"RSCS KALISUMUR BUMIAYU\n","Jenis":"Rumah Sunat"},"geometry":{"type":"Point","coordinates":[108.969359764058524,-7.256721003400754]}}]}
